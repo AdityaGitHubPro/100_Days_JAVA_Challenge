40c1206231d7b40e2728e1392862a07a68390850
AdityaGitHubPro <206301102@gkv.ac.in>
2023-04-01 19:10:55 +0530

    input

---
+++ commandLine argument03.java
@@ -0,0 +1,7 @@
+public class commandLineArgument03 {
+    public static void main(String[] args) {
+        for (String t : args) {
+            System.out.println(t);
+        }
+    }
+}
---
+++ printIntergerInJava02.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class printIntergerInJava02 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println("Number enter by you is " + num);
+    }
+}
